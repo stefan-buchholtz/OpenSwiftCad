2453a4a8fb3a9762f23ee73e958496c6a2d6069b
Stefan Buchholtz <stbuchholtz@gmail.com>
2015-03-14 18:49:04 +0100

    initial commit
    port of OpenJsCad to Swift

---
+++ OpenSwiftCad/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  OpenSwiftCad
+//
+//  Created by Stefan Buchholtz on 12.03.15.
+//  Copyright (c) 2015 Stefan Buchholtz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
